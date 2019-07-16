94656d9e212be47439bf74724223dc61ee93f8bd
vicg39 <vagarciamontiel@gmail.com>
2019-07-16 17:20:23 -0400

    design of the website and all configrations Js

---
+++ js/script.js
@@ -0,0 +1,101 @@
+$(document).ready(function() {
+    $('#slides').superslides({
+        animation: 'fade',
+        play: 4000,
+        pagination: false
+    });
+
+    let typed = new Typed(".typed", {
+        strings: ["Fullstack", "Web Developer"],
+        typeSpeed: 70,
+        loop: true,
+        startDelay: 1000,
+        showCursor: false
+    });
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        items: 4,
+        responsive:{
+            0:{
+                items:1
+            },
+            480:{
+                items:2
+            },
+            768:{
+                items:3
+            },
+            938:{
+                items:4
+            }
+        }
+    });
+
+    $('.chart').easyPieChart({
+        easing: 'easeInOut',
+        barColor: '#fff',
+        trackColor: false,
+        scaleColor: false,
+        lineWidth: 4,
+        size: 152,
+        onStep: function(from, to, percent) {
+            $(this.el).find('.percent').text(Math.round(percent));
+        }
+    });
+    
+
+    $("data-fancybox").fancybox();
+
+    $(".items").isotope({
+        filter: '*',
+        animationOptions:{
+            duration: 1500,
+            easing: 'linear',
+            queue: false,
+        }
+    });
+
+    $("#filters a").click(function() {
+        $("#filters .current").removeClass("current");
+        $(this).addClass("current"); 
+
+        let selector = $(this).attr("data-filter");
+
+        $(".items").isotope({
+            filter: selector,
+            animationOptions:{
+                duration: 1500,
+                easing: 'linear',
+                queue: false,
+            }
+        });
+
+        return false;
+    })
+
+    $("#navigation li a").click(function(e) {
+        e.preventDefault();
+
+        let targetElement = $(this).attr("href");
+        let targetPosition = $(targetElement).offset().top;
+        $("html, body").animate({scrollTop: targetPosition - 50}, "slow")
+    });
+
+    const nav = $("#navigation");
+    const navTop = nav.offset().top 
+
+    $(window).on("scroll", stickyNavigation);
+
+    function stickyNavigation() {
+        let body = $("body")
+
+        if($(window).scrollTop() >= navTop) {
+            body.css("padding-top", nav.outerHeight() + "px");
+            body.addClass("fixedNav");
+        } else {
+            body.css("padding-top", 0);
+            body.removeClass("fixedNav")
+        }
+    }
+});
